c68bd0f95b7e12db7548a5d0dc5c22e3a0090a12
karishma <karishma.kochar@novelvista.com>
2024-07-23 18:05:28 +0530

    Your commit message

---
+++ src/main/java/com/finace/FinancialPortfolioManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.finace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialPortfolioManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialPortfolioManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/finace/model/User.java
@@ -0,0 +1,5 @@
+package com.finace.model;
+
+public class User {
+
+}
